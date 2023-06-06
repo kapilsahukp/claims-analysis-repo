8bb3774160c2f78f2a16c33ba5256cd53d9de8c4
kapilsahukp <kapilsahu2102@gmail.com>
2023-06-06 14:18:19 -0400

    modified setup

--- setup.py
+++ setup.py
@@ -6,35 +6,29 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='claims_analysis_pkg',                           # should match the package folder
-    # packages=['claims_analysis_pkg', 'claims_analysis_pkg.src'],                     # should match the package folder
     packages=find_packages(),
     version='0.0.1',                                # important for updates
     license='MIT',                                  # should match your chosen license
     description='Testing installation of Package',
-    long_description=long_description,              # loads your README.md
-    long_description_content_type="text/markdown",  # README.md is of type 'markdown'
     author='Kapil Sahu',
     author_email='ksahu@charlesriverdata.com',
     url='https://github.com/kapilsahukp/claims-analysis-repo.git',
-    project_urls = {                                # Optional
-        "Bug Tracker": "https://github.com/kapilsahukp/claims-analysis-repo/issues"
-    },
     install_requires=["python-dotenv==1.0.0",
                       "openai>=0.27",
                       "langchain>=0.0.170",
                       "pandas", "pandas-stubs",
                       "ipykernel", "pypdf"
                       ],                  # list all packages that your package uses
-    classifiers=[                                   # https://pypi.org/classifiers
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Documentation',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
+    # classifiers=[                                   # https://pypi.org/classifiers
+    #     'Development Status :: 3 - Alpha',
+    #     'Intended Audience :: Developers',
+    #     'Topic :: Software Development :: Documentation',
+    #     'License :: OSI Approved :: MIT License',
+    #     'Programming Language :: Python :: 3',
+    #     'Programming Language :: Python :: 3.7',
+    #     'Programming Language :: Python :: 3.8',
+    #     'Programming Language :: Python :: 3.9',
+    # ],
 
-    download_url="https://github.com/kapilsahukp/claims-analysis-repo/archive/refs/tags/0.0.1.tar.gz",
+    # download_url="https://github.com/kapilsahukp/claims-analysis-repo/archive/refs/tags/0.0.1.tar.gz",
 )
