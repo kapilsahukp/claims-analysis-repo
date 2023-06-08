5f97a850a4ceab6bb260653ad88bb943c20ef6f3
kapilsahukp <kapilsahu2102@gmail.com>
2023-06-08 19:14:41 -0400

    Updated setup file

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='claims_analysis',                     # should match the package folder
     packages=find_packages(),
-    # version='0.0.1',                                # important for updates
-    # description='Testing installation of Package',
-    # author='Kapil Sahu',
-    # author_email='ksahu@charlesriverdata.com',
-    # url='https://github.com/kapilsahukp/claims-analysis-repo.git',
+    version='0.0.1',                            # important for updates
+    description='Testing installation of Package',
+    author='Kapil Sahu',
+    author_email='ksahu@charlesriverdata.com',
+    url='https://github.com/charles-river-data/neptune.git',
 
     # list all packages that the package uses
     install_requires=[
